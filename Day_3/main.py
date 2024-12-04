1d79554c619113aa640bbb804218df107024721a
Daniel Palma <71732131+DanyPalma@users.noreply.github.com>
2024-12-03 23:25:14 -0500

    Add day 3 finalized

--- Day_3/main.py
+++ Day_3/main.py
@@ -1,19 +1,29 @@
 import re
+import numpy as np
 
 def part1(filename: str) -> int: 
-    pattern = r'\bmul\(\d+,\d+\)\b'
+    pattern = r'(?<=mul\()\d{1,3},\d{1,3}(?=\))'
     sum_of_mul = 0
     
-    with open(filename) as file:
-        for line in file:
-            matches = re.findall(pattern, line)
-            for match in matches:
-                split  = re.search(r'mul\((\d+),(\d+)\)', match)
-                if split:
-                    numbers = [int(split.group(1)), int(split.group(2))]
-                    sum_of_mul += (numbers[0] * numbers[1])
-            
-        
+    matches = re.findall(pattern, open(filename).read())
+    # print(matches) 
+    
+    s = [int(op.split(",")[0]) * int(op.split(",")[1]) for op in matches]
+    
+    # print(s)
+    
+    sum_of_mul = sum(s)
+    
     return sum_of_mul
 
 print(part1('input.txt'))
+
+def part2(filename: str) -> int: 
+    code = open(filename).read()
+    insts = {x.span()[-1]: 0 for x in re.finditer(r'don\'t\(\)', code)}
+    dos = {x: 1 for x in [0] + [x.span()[-1] for x in re.finditer(r'do\(\)', code)]}
+    insts.update(dos)
+    
+    return sum([np.prod(np.array([int(x) for x in m.group().split(',')])) * insts[max([x for x in insts.keys() if x < m.span()[0]])] for m in re.finditer('(?<=mul\()\d{1,3},\d{1,3}(?=\))', code)])
+    
+print(part2('input.txt'))
